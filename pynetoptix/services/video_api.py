40c9587bce068705314496be6d767b31c73bbf4f
David Vuong <david.vuong256@gmail.com>
2018-01-10 14:20:45 +1100

    feat(VideoApi): add get_rtsp_stream_url

--- pynetoptix/services/video_api.py
+++ pynetoptix/services/video_api.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
+import urllib.parse
 
 
 class VideoApi:
     def __init__(self, config):
         self.config = config
+
+    def get_rtsp_stream_url(self, camera_id, **kwargs):
+        endpoint = f'{self.config.endpoint}/{camera_id}'
+        qs = {k: v for k, v in kwargs.items() if v is not None}
+        qs = urllib.parse.urlencode(qs)
+
+        return f'{endpoint}?{qs}' if qs else endpoint
