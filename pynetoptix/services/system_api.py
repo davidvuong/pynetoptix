9b07e25ff550e04690d012f059be1378dfe1fd41
David Vuong <david.vuong256@gmail.com>
2018-01-10 15:12:04 +1100

    fix(SystemApi): get_cameras_ex mv camera -> id

--- pynetoptix/services/system_api.py
+++ pynetoptix/services/system_api.py
@@ -10,5 +10,5 @@ class SystemApi:
         endpoint = f'{self.config.endpoint}/ec2/getCamerasEx'
 
         if camera_id is not None:
-            endpoint = f'{endpoint}?camera={camera_id}'
+            endpoint = f'{endpoint}?id={camera_id}'
         return http.get(endpoint, headers=self.config.headers)
