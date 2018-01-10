c6686a98e0b3d481ccae4ebf8fe28d09325609ef
David Vuong <david.vuong256@gmail.com>
2018-01-10 15:05:32 +1100

    tests+chore(test_server_api): add some basic pytests

---
+++ tests/test_server_api.py
@@ -0,0 +1,16 @@
+import pynetoptix
+import pytest
+
+
+class TestServerApi:
+    client = pynetoptix.create_client('demo', 'nxwitness', host='demo.networkoptix.com')
+
+    def test_ping_responds_with_pong(self):
+        assert self.client.server.ping()['error'] == '0'
+
+    def test_create_event_denies_empty_args(self):
+        with pytest.raises(ValueError):
+            self.client.server.create_event()
+
+    def test_create_event_allows_with_req_args(self):
+        assert self.client.server.create_event(source='camera_id')['error'] == '0'
