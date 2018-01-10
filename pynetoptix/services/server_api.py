4128cb61e54df875c9905d30b28fa8313f0a4c21
David Vuong <david.vuong256@gmail.com>
2018-01-10 14:31:28 +1100

    feat(ServerApi): add ping and create_event

--- pynetoptix/services/server_api.py
+++ pynetoptix/services/server_api.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
+from pynetoptix.core import http
 
 
 class ServerApi:
     def __init__(self, config):
         self.config = config
+
+    def ping(self):
+        return http.get(f'{self.config.endpoint}/api/ping', headers=self.config.headers)
+
+    def create_event(self, **kwargs):
+        endpoint = f'{self.config.endpoint}/api/createEvent'
+
+        if 'source' not in kwargs and 'caption' not in kwargs and 'description' not in kwargs:
+            raise ValueError('Either source, caption, or description must be set')
+        return http.get(endpoint, headers=self.config.headers)
