010c702931bbb656fda1780d2094cd6ed00c6612
David Vuong <david.vuong256@gmail.com>
2018-01-10 14:04:47 +1100

    feat(SystemApi): add get_cameras_ex

--- pynetoptix/services/system_api.py
+++ pynetoptix/services/system_api.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
+from pynetoptix.core import http
 
 
 class SystemApi:
     def __init__(self, config):
         self.config = config
+
+    def get_cameras_ex(self, camera_id=None):
+        endpoint = f'{self.config.endpoint}/ec2/getCamerasEx'
+
+        if camera_id is not None:
+            endpoint = f'{endpoint}?camera={camera_id}'
+        return http.get(endpoint, headers=self.config.headers)
