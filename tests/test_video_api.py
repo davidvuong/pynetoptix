94fb8cab966cd87f453984f2db711acf3384f8e4
David Vuong <david.vuong256@gmail.com>
2018-01-10 15:22:56 +1100

    refactor(test_video_api): pull host and port into vars

--- tests/test_video_api.py
+++ tests/test_video_api.py
@@ -4,9 +4,9 @@ import uuid
 
 class TestVideoApi:
     vms_host = 'demo.networkoptix.com'
-
-    client = pynetoptix.create_client('demo', 'nxwitness', host='demo.networkoptix.com')
+    vms_port = 7001
+    client = pynetoptix.create_client('demo', 'nxwitness', host=vms_host, port=vms_port)
 
     def test_get_rtsp_stream_url_allow_regular_camera_id(self):
         camera_id = uuid.uuid4()
-        assert self.client.video.get_rtsp_stream_url(camera_id) == f'http://{self.vms_host}:7001/{camera_id}'
+        assert self.client.video.get_rtsp_stream_url(camera_id) == f'http://{self.vms_host}:{self.vms_port}/{camera_id}'
