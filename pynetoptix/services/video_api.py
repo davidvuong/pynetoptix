9d9750e92b2a8a7e13a533486d5a1874c8b9ec9b
David Vuong <david.vuong256@gmail.com>
2018-01-15 10:41:31 +1100

    fix(video_api): fix broken qs encoding

--- pynetoptix/services/video_api.py
+++ pynetoptix/services/video_api.py
@@ -8,7 +8,6 @@ class VideoApi:
 
     def get_rtsp_stream_url(self, camera_id, **kwargs):
         endpoint = f'{self.config.endpoint}/{camera_id}'
-        qs = {k: v for k, v in kwargs.items() if v is not None}
-        qs = urllib.parse.urlencode(qs)
+        qs = '&'.join([f'{k}={urllib.parse.quote(v)}' for k, v in kwargs.items() if v is not None])
 
         return f'{endpoint}?{qs}' if qs else endpoint
