24ad489468383e9575e6994729b0cdbca8d240fc
David Vuong <david.vuong256@gmail.com>
2018-01-15 10:34:55 +1100

    test(test_server_api): add basic test for get_event_rules

--- tests/test_server_api.py
+++ tests/test_server_api.py
@@ -14,3 +14,6 @@ class TestServerApi:
 
     def test_create_event_allows_with_req_args(self):
         assert self.client.server.create_event(source='camera_id')['error'] == '0'
+
+    def test_get_event_rules_responds_with_rules(self):
+        assert isinstance(self.client.server.get_event_rules(), list)
