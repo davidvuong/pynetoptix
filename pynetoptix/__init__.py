54488a8bfe270fe191f9c07cc30de5a9f169ac4c
David Vuong <david.vuong256@gmail.com>
2018-03-29 11:09:54 +1100

    chore(setup): bump version 0.0.7, update author email

--- pynetoptix/__init__.py
+++ pynetoptix/__init__.py
@@ -6,11 +6,11 @@ from pynetoptix.services.system_api import SystemApi
 from pynetoptix.services.server_api import ServerApi
 from pynetoptix.services.video_api import VideoApi
 
-__version_info__ = (0, 0, 6)
+__version_info__ = (0, 0, 7)
 __version__ = '.'.join([str(i) for i in __version_info__])
 
 __author__ = 'David Vuong'
-__author_email__ = 'david.vuong256@gmail.com'
+__author_email__ = 'david@imageintelligence.com'
 
 
 def create_client(username, password, host='localhost', port=7001, protocol='http'):
